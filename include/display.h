dc55789dffa30dfa29beb862ad32b260efc7fa86
Italo Di Renzo <i.direnzo@vehikl.com>
2020-11-19 17:18:54 -0500

    Remove display_poll_events() from display.h

--- include/display.h
+++ include/display.h
@@ -26,7 +26,6 @@ typedef enum input_key
 void display_init();
 void display_quit();
 void display_event_loop(struct ch8_cpu *cpu);
-void display_poll_events();
 void display_clear();
 void draw_sprite(struct ch8_cpu *cpu, uint8_t x, uint8_t y, uint8_t h);
 
